5b4d2f577f22b8c27a406a9ba93f277d4a688ce3
Anne McCorkle <annefmccorkle@gmail.com>
2022-07-12 15:47:17 -0600

    unique id successfully posting

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,11 @@
 const notes = require("express").Router();
 //unique id for each note
-const uuid = require('../helpers/uuid')
+const { v4:uuidv4 } = require('uuid')
 //using helper functions
-const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, 
+    writeToFile, 
+    readAndAppend 
+    } = require('../helpers/fsUtils');
 
 
 //route for retrieving rendered notes
@@ -15,11 +18,11 @@ notes.get('/', (req, res) => {
 notes.post('/', (req, res) => {
     //destructuring the json object
     const { title, text } = req.body;
-    if(title && text) {
+    if(req.body) {
         const newNote = {
             title,
             text,
-            id: uuid(),
+            id: uuidv4(),
         };
         readAndAppend(newNote, "./db/db.json");
         res.json(`New note successfully added`);
